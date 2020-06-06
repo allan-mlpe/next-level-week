51eb66cd0a967306fd8f343bca8cba46d5454729
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2020-06-06 17:40:15 -0300

    Configurando o roteamento da aplicação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home';
+
+import Router from './routes'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router />
     </div>
   );
 }
