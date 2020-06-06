64ad397d9d6c7952cc1c2e0625cd3bac548f19ef
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2020-06-06 18:34:37 -0300

    Integrando o mapa no formulário

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -62,6 +63,17 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <div id="map">
+                        <Map center={[-8.0277473,-34.8955087]} zoom={15}>
+                            <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+
+                            <Marker position={[-8.0277473,-34.8955087]} />
+                        </Map>
+                    </div>
 
                     <fieldset>
                         <div className="field-group">
